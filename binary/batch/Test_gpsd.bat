7ab7885fa67d88f722c5cacbff2d8ab88d7c6121
heldersepu <heldersepu@36feb3bc-8865-11de-82e5-a1837a67dc72>
2009-08-16 13:51:21 +0000

    Add coments

--- binary/batch/Test_gpsd.bat
+++ binary/batch/Test_gpsd.bat
@@ -1,3 +1,8 @@
+:: ** This Batch file will launch gpspipe from the cygwin folder **
+:: gpspipe is a simple client that captures GPS output and/or gpsd 
+:: reports and sends it to standard output.
+::
+
 @COLOR 0A
 @ECHO off
 
