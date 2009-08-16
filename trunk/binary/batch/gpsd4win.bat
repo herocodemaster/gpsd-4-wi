1d59017e8e8eb92e4e06790c52d41628c7d050db
heldersepu <heldersepu@36feb3bc-8865-11de-82e5-a1837a67dc72>
2009-08-16 13:51:21 +0000

    Add coments

--- trunk/binary/batch/gpsd4win.bat
+++ trunk/binary/batch/gpsd4win.bat
@@ -1,7 +1,18 @@
+:: ** This Batch file will launch gpsd from the cygwin folder **
+:: gpsd is a daemon that monitors one or more GPSes attached to a 
+:: host computer through serial or USB ports, making all data on the 
+:: location/course/velocity of the sensors available to be queried 
+:: on TCP port 2947 of the host computer.
+::
+:: You need to the COM port that your GPS receiver uses.
+:: This batch file used com5 by default
+::
+
 @COLOR 0A
 @ECHO off
 
 %SystemDrive%
 CHDIR %SystemDrive%\cygwin\bin
 
+:: Replace com5 with your own COM port below
 gpsd -b -N -D 2 /dev/com5 
