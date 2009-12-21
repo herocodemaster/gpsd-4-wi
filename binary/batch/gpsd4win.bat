4341c7bb11e327405de151c9a6fe84eebbe97b86
heldersepu <heldersepu@36feb3bc-8865-11de-82e5-a1837a67dc72>
2009-12-21 02:16:14 +0000

    the command: REG QUERY %regKey% /f COM%PortNum% /d does not work in XP

--- binary/batch/gpsd4win.bat
+++ binary/batch/gpsd4win.bat
@@ -1,7 +1,7 @@
 :: ** This Batch file will launch gpsd from the cygwin folder **
-:: gpsd is a daemon that monitors one or more GPSes attached to a 
-:: host computer through serial or USB ports, making all data on the 
-:: location/course/velocity of the sensors available to be queried 
+:: gpsd is a daemon that monitors one or more GPSes attached to a
+:: host computer through serial or USB ports, making all data on the
+:: location/course/velocity of the sensors available to be queried
 :: on TCP port 2947 of the host computer.
 ::
 :: You need to use the COM port that your GPS receiver is using.
@@ -16,25 +16,34 @@ SET PortNum=0
 
 IF NOT %PortNum%==0 GOTO launchGPSD
 SET regKey=HKLM\HARDWARE\DEVICEMAP\SERIALCOMM
+SET strFile=SerialCOMM.txt
+REG QUERY %regKey% > %strFile%
 
 :: Test a few of the most common ports
+:: in case of multiple options last one takes priority
 :preferiti
-    FOR %%I IN (5 4 1 3) DO REG QUERY %regKey% /f COM%%I /d | IF %ErrorLevel%==0 SET PortNum=%%I
+    SET PortNum=4
+    FIND "COM%PortNum%" %strFile% || SET PortNum=0
+    SET PortNum=3
+    FIND "COM%PortNum%" %strFile% || SET PortNum=0
+    SET PortNum=5
+    FIND "COM%PortNum%" %strFile% || SET PortNum=0
     IF NOT %PortNum%==0 GOTO launchGPSD
 
 :: Search all ports from 1 to 255
 :repeat
     CLS
-    SET /A PortNum=%PortNum% + 1 
+    SET /A PortNum=%PortNum% + 1
     IF %PortNum%==256 GOTO errorNoPortFound
-    REG QUERY %regKey% /f COM%PortNum% /d || GOTO repeat
+    FIND "COM%PortNum%" %strFile% || GOTO repeat
 
 :: Launch GPSD with the given port number
 :launchGPSD
     IF %PortNum%==0 GOTO errorNoPortFound
     %SystemDrive%
     CHDIR %SystemDrive%\cygwin\bin
-    gpsd -b -N -D 2 /dev/com%PortNum% 
+    ECHO.
+    gpsd -b -N -D 2 /dev/com%PortNum%
 
 GOTO EOF
 
@@ -43,7 +52,7 @@ GOTO EOF
     COLOR 0C
     ECHO.
     ECHO.
-    ECHO  No Serial Port detected! 
+    ECHO  No Serial Port detected!
     ECHO  Make sure your GPS is properly connected and try again.
     ECHO.
     ECHO.
